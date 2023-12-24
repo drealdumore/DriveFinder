1efe205f2866e709a0eb3feb4a719caae1d82e42
drealdumore <sireroyce1@gmail.com>
2023-12-24 01:17:10 +0100

    Added auth to the nav

--- src/app/components/user-sub/user-sub.component.ts
+++ src/app/components/user-sub/user-sub.component.ts
@@ -1,13 +1,38 @@
 import { Component, Input } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'user-sub',
   templateUrl: './user-sub.component.html',
-  styleUrls: ['./user-sub.component.css']
+  styleUrls: ['./user-sub.component.css'],
 })
 export class UserSubComponent {
-  @Input() userData: [] = []
+  @Input() userData: [] = [];
+  @Input() userEmail: string = '';
+  @Input() userName: string = '';
+  @Input() userImg: string = '';
 
-  
+  constructor(
+    private authService: AuthService,
+    private toastr: ToastrService,
+    private modalService: ModalService
+  ) {}
+
+  logout() {
+    this.authService
+      .logOut()
+      .then((res: any) => {
+        this.toastr.success('Successfully Logged Out.');
+      })
+      .catch((error: any) => {
+        this.toastr.error('error');
+      });
+  }
+
+  close() {
+    this.modalService.closeModal();
+  }
 
 }
--- src/app/newnav/newnav.component.ts
+++ src/app/newnav/newnav.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'navigation',
   templateUrl: './newnav.component.html',
   styleUrls: ['./newnav.component.css'],
 })
-export class NewnavComponent {
+export class NewnavComponent implements OnInit {
   isAuthenticated: boolean = false;
   Sub: boolean = false;
   isDropdownVisible = false;
+  user: string = '';
+  userImg: string = '';
+  userEmail: string = '';
 
-  constructor(public modalService: ModalService) {}
+  constructor(
+    public modalService: ModalService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    // to check authentication state
+    this.authService.isAuthenticated$.subscribe((isAuthenticated) => {
+      this.isAuthenticated = isAuthenticated;
+    });
+
+    // to get user display name and img from their email
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      this.user = JSON.parse(storedUser).displayName;
+      this.userImg = JSON.parse(storedUser).photoURL;
+      this.userEmail = JSON.parse(storedUser).email;
+    }
+  }
 
   openModal() {
     this.modalService.openModal();
